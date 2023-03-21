d99b2f731834b6c7d41a67775e348482ac5e5c39
josed5001 <joderas@s.sfusd.edu>
2023-03-21 09:33:21 -0700

    Unit 5.1 Complete

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -10,7 +10,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -2;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +29,15 @@ public class Target : MonoBehaviour
         
     }
 
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
