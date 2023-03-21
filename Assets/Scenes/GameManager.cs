80b908b0015918e9d1654c79ed371c082b66c317
josed5001 <joderas@s.sfusd.edu>
2023-03-21 09:46:19 -0700

    Unit 5.2

--- Assets/Scenes/GameManager.cs
+++ Assets/Scenes/GameManager.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
     public List<GameObject> targets;
+    public TextMeshProUGUI scoretext;
+    private int score;
     private float spawnRate = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+
     }
 
     // Update is called once per frame
@@ -28,8 +34,14 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            UpdateScore(5);
         }
     }
-        
+    
+    void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoretext.text = "Score: " + score;
+    }
 }  
 
