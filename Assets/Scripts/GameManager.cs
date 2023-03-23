be32177cf45e09c98a278b5742066b0927163971
josed5001 <joderas@s.sfusd.edu>
2023-03-23 11:56:38 -0700

    Unit 5.2 Complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -34,11 +34,10 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
-            UpdateScore(5);
         }
     }
     
-    void UpdateScore(int scoreToAdd)
+    public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
         scoretext.text = "Score: " + score;
